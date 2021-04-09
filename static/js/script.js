049ab3218007892e3c81b892f1f3d9f14492d45c
wyne-ybanez <wyne_01@icloud.com>
2021-04-09 18:08:09 +0000

    Add JS interactivity to navbar and small style edits

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+
+    // Transparent Scroll Effect of Navbar from - Editted: https://stackoverflow.com/questions/29646622/set-bootstrap-navbar-transparency-on-scroll
+    function checkScroll(){
+        var startY = $('.navbar').height() * 2; 
+
+        if($(window).scrollTop() > startY){
+            $('.navbar').addClass("scrolled");
+            $('.navbar').removeClass("transparent");
+        }else{
+            $('.navbar').addClass("transparent");
+            $('.navbar').removeClass("scrolled");
+        }
+    }
+
+    if($('.navbar').length > 0){
+        $(window).on("scroll load resize", function(){
+            checkScroll();
+        });
+    }
 });
