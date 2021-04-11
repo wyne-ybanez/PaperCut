72e262177630f028d09af50e87219edc2203a4b3
wyne-ybanez <wyne_01@icloud.com>
2021-04-11 01:33:47 +0000

    Add session user's profile

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def register():
 
         session['user'] = request.form.get('username').lower()
         flash('Registration Successful!')
+        return redirect(url_for('profile', username=session['user']))
     return render_template('register.html')
 
 
@@ -57,7 +58,7 @@ def register():
 def login():
     '''
     Check if username exists in db
-    Check if password matches with username
+    Check if hashed password matches with username
     '''
     if request.method == 'POST':
         existing_user = mongo.db.users.find_one(
@@ -69,6 +70,8 @@ def login():
                     session['user'] = request.form.get('username').lower()
                     flash('Welcome, {}'.format(
                         request.form.get('username')))
+                    return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 flash('Incorrect Username and/or Password')
                 return redirect(url_for("login"))
@@ -78,6 +81,17 @@ def login():
             return redirect(url_for("login"))
 
     return render_template('login.html')
+
+
+@app.route('/profile/<username>', methods=['GET','POST'])
+def profile(username):
+    '''
+    Create profile page for user
+    Taking user's username from DB
+    '''
+    username = mongo.db.users.find_one(
+        {'username': session['user']})['username']
+    return render_template('profile.html', username=username)
 
 
 if __name__ == '__main__':
