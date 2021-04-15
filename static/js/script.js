0600c6cca0e0373dfc7bb0bd4318968b1384f554
wyne-ybanez <wyne_01@icloud.com>
2021-04-15 14:56:49 +0000

    Add further website styling

--- static/js/script.js
+++ static/js/script.js
@@ -4,21 +4,21 @@ $(document).ready(function () {
     $("select").formSelect();                      // Form select
 
     // Scroll effect interactivity - Editted: https://stackoverflow.com/questions/29646622/set-bootstrap-navbar-transparency-on-scroll
-    function checkScroll(){
-        var startY = $('.navbar').height() * 2; 
+    // function checkScroll(){
+    //     var startY = $('.navbar').height() * 2; 
 
-        if($(window).scrollTop() > startY){
-            $('.fa-bars').addClass("scrolled");
-        }else{
-            $('.fa-bars').removeClass("scrolled");
-        }
-    }
+    //     if($(window).scrollTop() > startY){
+    //         $('.fa-bars').addClass("scrolled");
+    //     }else{
+    //         $('.fa-bars').removeClass("scrolled");
+    //     }
+    // }
 
-    if($('.navbar').length > 0){
-        $(window).on("scroll load resize", function(){
-            checkScroll();
-        });
-    }    
+    // if($('.navbar').length > 0){
+    //     $(window).on("scroll load resize", function(){
+    //         checkScroll();
+    //     });
+    // }    
 
     // Code Institute Validation Code
     validateMaterializeSelect();
